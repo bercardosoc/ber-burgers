25c38ae8243b363a7d22bfaeee1312166e81a395
Bernardo <bernardocardosodacosta@gmail.com>
2022-01-21 12:30:45 -0300

    style: change add and sub buttons

--- src/components/CartProduct/index.tsx
+++ src/components/CartProduct/index.tsx
@@ -1,4 +1,4 @@
-import { Flex, Image, Text } from "@chakra-ui/react";
+import { Button, Flex, Image, Text } from "@chakra-ui/react";
 import { DeleteIcon } from "@chakra-ui/icons"
 import { useCart } from "../../providers/Cart";
 
@@ -17,16 +17,16 @@ export const CartProduct = (item: Cart) => {
     const { addQuantity, subQuantity, deleteFromCart } = useCart()
 
     return (
-        <Flex border="solid 1px" borderColor={"orange.500"} borderRadius={"10px"} width={"90%"} margin="0.3rem auto"  >
+        <Flex border="solid 1px" borderColor={"orange.500"} borderRadius={"10px"} width={"90%"} margin="0.3rem auto">
             <Flex alignItems={"center"} key={item.id} >
-                <Image src={item.img} height={"120px"} />
-                <Flex flexDirection={"column"} >
-                    <Text>{item.name} </Text>
+                <Image src={item.img} width={"125px"} />
+                <Flex marginLeft={"1.5rem"} flexDirection={"column"} textAlign={"center"} >
+                    <Text fontWeight={"bold"} >{item.name} </Text>
                     <Text>${item.price}</Text>
-                    <Text>{item.quantity}</Text>
-                    <Flex>
-                        <button onClick={() => addQuantity(item) } >+</button>
-                        <button onClick={() => subQuantity(item)} >-</button>
+                    <Text>Quantity: {item.quantity}</Text>
+                    <Flex margin="0 auto">
+                        <Button marginRight={"0.5rem"} size={"sm"} bg={"green"} onClick={() => addQuantity(item) } >+</Button>
+                        <Button marginLeft={"0.5.5rem"} size={"sm"} bg={"red"} onClick={() => subQuantity(item)} >-</Button>
                     </Flex>
                 </Flex>
             </Flex>
--- src/providers/Cart.tsx
+++ src/providers/Cart.tsx
@@ -1,5 +1,4 @@
-import { AxiosResponse } from "axios"
-import { createContext, ReactNode, useCallback, useContext, useEffect, useState } from "react"
+import { createContext, ReactNode, useContext, useEffect, useState } from "react"
 import { api } from "../services/api"
 import { useAuth } from "./Auth"
 
@@ -32,10 +31,6 @@ interface CartContextData {
     subQuantity: (product: Cart) => void
     deleteFromCart: (product: Cart) => void
     deleteAll: () => void
-    /* cart: Product[]
-    addToCart: (data: Omit<Product, "id">, accessToken: string) => Promise<void>
-    loadCart: (userId: string, accessToken: string) => Promise<void>
-    deleteFromCart: (itemIndex: string, accessToken: string) => Promise<void> */
 }
 
 const CartContext = createContext<CartContextData> ({} as CartContextData)
@@ -57,7 +52,7 @@ const CartProvider = ({ children }: CartProviderProps ) => {
 
     const addToCart = (product: Product) => {
         if (cart.filter((item) => item.name === product.name).length === 0) {
-            const sendToCart = {...{product}, ...{quantity: 1, userId: user.id }}
+            const sendToCart = {...{category: product.category, img: product.img, name: product.name, price: product.price}, ...{quantity: 1, userId: user.id }}
             api
             .post("/itens", sendToCart, {
                 headers: {
@@ -125,52 +120,6 @@ const CartProvider = ({ children }: CartProviderProps ) => {
         })
         .catch(err => setCart(cart))
     }, [refresh])
-
-    /* const addToCart = useCallback(
-        async (data: Omit <Product, "id">, accessToken: string) => {
-            api
-            .post("/itens", data, {
-                headers: {
-                    Authorization: `Bearer ${accessToken}`,
-                },
-            })
-            .then((response: AxiosResponse<Product>) => 
-                setCart((oldState) => [...oldState, response.data])
-                )
-                .catch((err) => console.log(err)) 
-        },
-        []
-    ) */
-
-    /* const deleteFromCart = useCallback(
-        async (itemIndex: string, accessToken: string) => {
-            await api
-            .delete(`/cart/${itemIndex}`, {
-                headers: {
-                    Authorization: `Bearer ${accessToken}`,
-                },
-            })
-            .then((_) => {
-                const filteredCart = cart.filter((e) => e.id !== Number(itemIndex))
-                setCart(filteredCart)
-            })
-            .catch((err) => console.log(err))
-        },
-        [cart]
-    ) */
-
-    /* const loadCart = useCallback(async(userId: string, accessToken: string) => {
-        try {
-            const response = await api.get(`/cart?userId=${userId}`, {
-                headers: {
-                    Authorization: `Bearer ${accessToken}`,
-                },
-            })
-            setCart(response.data)
-        } catch (err) {
-            console.log(err)
-        }
-    }, []) */
 
     return (
         <CartContext.Provider
